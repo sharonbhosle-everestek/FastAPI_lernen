1a1d481cb9cd8c311de6575faaaa30612d0c485e
Sharon Bhosle <sharon.bhosle@everestek.com>
2026-02-10 16:55:21 +0530

    closing in

--- certificates_dto.py
+++ certificates_dto.py
@@ -1,23 +1,23 @@
-    from pydantic import BaseModel
-    from typing import Optional
+from pydantic import BaseModel
+from typing import Optional
 
-    class Certification(BaseModel):
-        id: Optional[str]
-        resource_id: Optional[str]
-        name: Optional[str]
-        # level: Optional[CertificationEnum]
-        validation_date: Optional[str]
+class Certification(BaseModel):
+    id: Optional[str]
+    resource_id: Optional[str]
+    name: Optional[str]
+    # level: Optional[CertificationEnum]
+    validation_date: Optional[str]
 
-    def dto_2_db_convert(certification_dto: Certification):
+def dto_2_db_convert(certification_dto: Certification):
 
-        print("====")
-        db_resource_certifications_list = []
-        for certification in certification_dto:
-            json_certifications = certification.dict()
-            print(json_certifications, type(json_certifications))
-        #     db_resource_certifications = db.ResourceCertification(**json_certifications)
-        #     db_resource_certifications.resource_id = resource_id
-        #     db_resource_certifications_list.append(db_resource_certifications)
-        # return db_resource_certifications_list
+    print("====")
+    db_resource_certifications_list = []
+    for certification in certification_dto:
+        json_certifications = certification.dict()
+        print(json_certifications, type(json_certifications))
+    #     db_resource_certifications = db.ResourceCertification(**json_certifications)
+    #     db_resource_certifications.resource_id = resource_id
+    #     db_resource_certifications_list.append(db_resource_certifications)
+    # return db_resource_certifications_list
 
-    dto_2_db_convert(Certification(*))
+# dto_2_db_convert(Certification(*))
