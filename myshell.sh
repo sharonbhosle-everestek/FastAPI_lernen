d9eecce5b906eb0db9555b82a4f7aa45a9fdf320
Sharon Bhosle <sharon.bhosle@everestek.com>
2026-02-12 11:59:51 +0530

    added a shell file

---
+++ myshell.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+echo "This is sharon here" 
+a=4
+
+if [ $1 -gt 3 ]; then 
+    echo "If statment runned" 
+else 
+    echo "else statment"
+fi
+
+
+for i in $s; do 
+    echo "for loop running Something $i"
+done
+
+s=( 1,2,3,4 )
+echo $s
+
+
+
